a930a8e4f0bd31c03a3f018bbd9300f27ff85b41
mohammed benyamna <mohammedbenyamna39@gmail.com>
2025-05-04 12:12:27 +0100

    update the requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     install_requires=[
         "scikit-learn",
         "nltk",
+        "pandas",
         "sphinx",
         "pytest",  # Usually listed under extras, but fine here too
     ],
