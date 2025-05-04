2967290eb9cb84bf713d8b5f7d2b1005aead0ee1
mohammed benyamna <mohammedbenyamna39@gmail.com>
2025-05-04 11:38:19 +0100

    add the dev files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textpipe",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "scikit-learn",
+        "nltk",
+        "sphinx",
+        "pytest",  # Usually listed under extras, but fine here too
+    ],
+    author="Your Name",
+    description="all in one tool for text tokenization and vectorization",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
